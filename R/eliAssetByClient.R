17fd40643fa297b0076da7cd1931de19ae213c62
silin yuan <silin yuan>
2016-11-17 19:42:40 +0800

    OW-168, update r file

--- R/eliAssetByClient.R
+++ R/eliAssetByClient.R
@@ -9,10 +9,10 @@ eliAssetByClient <- function(cientId){
   # graph = startGraph("http://neo4j:7474/db/data")
   graph = startGraph("http://localhost:7474/db/data/")
   
-#  file.url <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/Cypher/eliAssetByClient.cql'
+  file.url <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/Cypher/eliAssetByClient.cql'
   
-#  query <- readLoad(file.url)
-  query <- readLoad("eliAssetByClient.cql")
+  query <- readLoad(file.url)
+  
   cypher(graph,query,clientId=clientId)
 }
 
