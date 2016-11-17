b482daf1e0fac0ca054012b3f833317250451b14
silin yuan <silin yuan>
2016-11-17 18:56:35 +0800

    OW-168, wrote cypher to request DB, use R to run cypher

---
+++ R/eliAssetByClient.R
@@ -0,0 +1,20 @@
+library('RNeo4j')
+
+eliAssetByClient <- function(cientId){
+  readLoad <- function(path) {
+    query = paste(readLines(path), collapse="\n")
+    return (query)
+  }
+  
+  # graph = startGraph("http://neo4j:7474/db/data")
+  graph = startGraph("http://localhost:7474/db/data/")
+  
+  file.url <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/Cypher/accAssetByClient.cql'
+  
+  query <- readLoad(file.url)
+  
+  cypher(graph,query,clientId=clientId)
+}
+
+clientId <- 'c1'
+result <- eliAssetByClient(clientId)
