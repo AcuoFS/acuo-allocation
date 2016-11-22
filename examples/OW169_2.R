548da07ce0f935d7becf94bddcfc60a7a951543c
silin yuan <silin yuan>
2016-11-22 18:45:03 +0800

    OW-169, update exaples

--- examples/OW169_2.R
+++ examples/OW169_2.R
@@ -2,7 +2,7 @@ library('RNeo4j')
 
 source('src/availAssetByCall.R')
 
-callId <- c('mc1')
+callId <- c('mc2','mc3')
 order <- 'assetId'
 
 result <- availAssetByCall(callId,order)
--- examples/OW169_3.R
+++ examples/OW169_3.R
@@ -2,7 +2,7 @@ library('RNeo4j')
 
 source('src/availAssetByCall.R')
 
-callId <- c('mc1')
+callId <- c('mc1','mc2','mc4','mc5','mc6','mc8','mc9','mc10','mc11')
 order <- 'assetId'
 
 result <- availAssetByCall(callId,order)
