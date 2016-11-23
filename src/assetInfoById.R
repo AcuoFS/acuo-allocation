d2dccfef1691245d6488ba6b355cb8f1372481d2
silin yuan <silin yuan>
2016-11-23 19:21:57 +0800

    OW-200, update assetInfoById.R

--- src/assetInfoById.R
+++ src/assetInfoById.R
@@ -6,8 +6,8 @@ assetInfoById <- function(assetId){
     return (query)
   }
   
-  # graph = startGraph("http://neo4j:7474/db/data")
-  graph = startGraph("http://localhost:7474/db/data/")
+  graph = startGraph("http://neo4j:7474/db/data")
+  #graph = startGraph("http://localhost:7474/db/data/")
   
   file.url <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/src/Cypher/assetInfoById.cql'
   
