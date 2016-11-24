7dc37397b617b52f806522266fd2fc8aa4d0ba5b
silin yuan <silin yuan>
2016-11-24 18:30:51 +0800

    OW-200, change the file names, update test function upon the suggestion

---
+++ src/callInfoByCallId.R
@@ -0,0 +1,18 @@
+library('RNeo4j')
+
+callInfoById <- function(callId){
+  readLoad <- function(path) {
+    query = paste(readLines(path), collapse="\n")
+    return (query)
+  }
+  
+  #graph = startGraph("http://neo4j:7474/db/data")
+  graph = startGraph("http://localhost:7474/db/data/")
+  
+  file.url <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/src/Cypher/callInfoById.cql'
+  
+  query <- readLoad(file.url)
+  
+  cypher(graph,query,callId=callId)
+}
+
