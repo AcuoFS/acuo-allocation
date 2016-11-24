be2b5fad294c5da0f5772b69a488983b82b2583b
silin yuan <silin yuan>
2016-11-24 11:33:15 +0800

    OW-171, restructure the examples code

--- examples/OW169_3.R
+++ examples/OW169_3.R
@@ -3,6 +3,6 @@ library('RNeo4j')
 source('src/availAssetByCall.R')
 
 callId <- c('mc1','mc2','mc4','mc5','mc6','mc8','mc9','mc10','mc11')
-order <- 'assetId'
+order <- 'callId'
 
 result <- availAssetByCall(callId,order)
