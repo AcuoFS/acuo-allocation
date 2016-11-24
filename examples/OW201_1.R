38bfca0116abe1cee1a63d0776e4e75195a5b7f4
silin yuan <silin yuan>
2016-11-24 15:42:37 +0800

    OW-201,update example OW201_1.R

--- examples/OW201_1.R
+++ examples/OW201_1.R
@@ -2,6 +2,6 @@ library('RNeo4j')
 
 source('src/callInfoById.R')
 
-callId <- c("mc1","mc2","mc4","mc5","mc6","mc8","mc9","mc10","mc11")
+callId <- c("mc1","mc2","mc3","mc4","mc5","mc6","mc8","mc9","mc10","mc11")
 
 result <- callInfoById(callId)
