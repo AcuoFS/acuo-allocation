03fdd3d03da164cd15db25c84ac3900f89dfcbe7
silin yuan <silin yuan>
2016-11-24 18:37:20 +0800

    OW-201, add test function

--- test/testFunctions/TestCallInfoByCallId.R
+++ test/testFunctions/TestCallInfoByCallId.R
@@ -1,6 +1,6 @@
 library('RUnit')
 
-test.OW201_1 <- function(){
+test.OW201_1 <- function(){ 
   
   # test input: a list of margin call ids
   callId <- c("mc1","mc2","mc3","mc4","mc5","mc6","mc7","mc8","mc9","mc10","mc11")
