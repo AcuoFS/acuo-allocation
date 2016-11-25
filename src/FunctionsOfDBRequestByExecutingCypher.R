022e57fb2eb708e639f8555951b93ab01f7472cd
silin yuan <silin yuan>
2016-11-25 11:25:39 +0800

    OW-168,OW-169,OW-200,OW-201, integrate all R files related to DB request in one

---
+++ src/FunctionsOfDBRequestByExecutingCypher.R
@@ -0,0 +1,34 @@
+library('RNeo4j')
+
+neo4jUrl <- 'http://neo4j:7474/db/data'
+neo4jLocalUrl <- 'http://localhost:7474/db/data/'
+
+callInfoByCallIdCypherPath <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/src/Cypher/callInfoByCallId.cql'
+availAssetByCallIdCypherPath <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/src/Cypher/availAssetByCallId.cql'
+eliAssetByClientIdCypherPath <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/src/Cypher/eliAssetByClientId.cql'
+assetInfoByAssetIdCypherPath <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/src/Cypher/assetInfoByAssetId.cql'
+
+
+executeCypher <- function(path,...){
+  params <- list(...)
+  query = paste(readLines(path), collapse="\n")
+  graph = startGraph(neo4jUrl)
+  #graph = startGraph(neo4jLocalUrl)
+  cypher(graph,query,params)
+}
+  
+callInfoByCallId <- function(callId){
+  executeCypher(path=callInfoByCallIdCypherPath,callId=callId)
+}
+
+assetInfoByAssetId <- function(assetId){
+  executeCypher(path=assetInfoByAssetIdCypherPath,assetId=assetId)
+}
+
+eliAssetByClientId <- function(clientId){
+  executeCypher(path=eliAssetByClientIdCypherPath,clientId=clientId)
+}
+
+availAssetByCallId <- function(callId,order='assetId') {
+  executeCypher(path=availAssetByCallIdCypherPath, callId=callId, order=order)
+}
