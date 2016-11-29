988e32d0bf7e6b7c2ffd93dc0b1ca887bfba3056
silin yuan <silin yuan>
2016-11-29 15:52:04 +0800

    OW-171, embed the lpSolve into the allocation function

--- src/allocationFunction.R
+++ src/allocationFunction.R
@@ -14,12 +14,12 @@ allocationAlgo <- function(callId='mc1',clientId='c1',order='assetId',pref=c(0,0
   call.num <- length(callId)
   asset.num <- length(assetId)
   
-  eli.mat <- input.list$eli.mat
-  haircut.mat<-input.list$haircut.mat
-  quantity.mat<- input.list$quantity.mat
-  value.mat<-input.list$value.mat
-  call.mat <- input.list$call.mat
-  cost.percent.mat <- input.list$cost.mat
+  eli.mat <- input.list$eli.mat; eli.vec <- input.list$eli.vec
+  haircut.mat<-input.list$haircut.mat; haircut.vec <- input.list$haircut.vec
+  quantity.mat<- input.list$quantity.mat; quantity.vec <- input.list$quantity.vec
+  value.mat<-input.list$value.mat; value.vec <- input.list$value.vec
+  call.mat <- input.list$call.mat;
+  cost.percent.mat <- input.list$cost.mat; cost.vec <- input.list$cost.vec
 
 ############### Output Format ###########################
   output.list <- list()
@@ -50,6 +50,7 @@ if(all(pref==c(0,0,1,0))){  # In case of OW-171,173,174, pref=(0,0,1,0)
     errorMsg <- 'Asset inventoty is not sufficient!'
     return(errorMsg)
   }
+  
   ######### SORT ASSET PER CALL BY COST ########################
   cost.mat<-call.mat/(1-haircut.mat)*cost.percent.mat  # cost amount
   
@@ -97,12 +98,74 @@ if(all(pref==c(0,0,1,0))){  # In case of OW-171,173,174, pref=(0,0,1,0)
     output.list<- select.list
   }
   
-  else if(1){
+  else if(1){ #In case of OW-174, all assets have quantity limits
+    ###### USE THE LIBRARY 'lpSolve' #######################
+    # variables: x[1:(call.num*asset.num)] qunatity used of each asset for each margin call
+    # objective function: minimize  x*value/(1-haircut)*cost
+    # constraints: 
+    # 1. quantity limit of each asset for one margin call (call.num*asset.num)
+    #    quantity used <= quantity limit; 0 for non-eligible
+    # 2. quantity limit of each asset for all margin calls(asset.num)
+    # total quantity used <= total quantity (for an asset)
+    # 3. margin call requirement (call.num)
+    # total net amount of assets for one margin call >= call amount
+    
+    idx.eli <- which(eli.vec==1)
+    var.num <- length(idx.eli)
+    
+  #  old.var.num <- call.num*asset.num
+    
+    f.con.1 <- matrix(0,nrow=var.num,ncol=var.num)
+    f.con.1[cbind(1:var.num,1:var.num)] <- 1
+    f.dir.1 <- rep('<=',var.num)
+  #  old.f.dir.1[which(eli.vec==0)] <- '='
+    f.rhs.1 <- eli.vec[idx.eli]*quantity.vec[idx.eli]
+    
+    f.con.2 <- matrix(0,nrow=asset.num,ncol=var.num)
+    temp1 <- 1+(0:(call.num-1))*asset.num
+    idx.con.2 <- rep(temp1,asset.num)+rep(c(0:(asset.num-1)),rep(call.num,asset.num))
+    idx.con.2 <- match(idx.con.2,idx.eli)
+  #  old.f.con.2[cbind(rep(c(1:asset.num),rep(call.num,asset.num)),idx.con.2)]<-1
+    f.con.2[na.omit(cbind(rep(c(1:asset.num),rep(call.num,asset.num)),idx.con.2))]<-1
+    f.dir.2 <- rep('<=',asset.num)
+    f.rhs.2 <- quantity.mat[1,]
+    
+    f.con.3 <- matrix(0,nrow=call.num,ncol=var.num)
+    idx.con.3 <- 1:(asset.num*call.num)
+    idx.con.3 <- match(idx.con.3,idx.eli)
+  #  old.f.con.3[cbind(rep(c(1:call.num),rep(asset.num,call.num)),idx.con.3)]<- value.vec
+    f.con.3[na.omit(cbind(rep(c(1:call.num),rep(asset.num,call.num)),idx.con.3))] <- value.vec[idx.eli]*(1-haircut.vec[idx.eli])
+    f.dir.3 <- rep('>=',call.num)
+    f.rhs.3 <- call.mat[,1]
+    
+    f.obj <-  value.vec[idx.eli]/(1-haircut.vec[idx.eli])*cost.vec[idx.eli]
+    f.con <- rbind(f.con.1,f.con.2,f.con.3)
+    f.dir <- c(f.dir.1,f.dir.2,f.dir.3)
+    f.rhs <- c(f.rhs.1,f.rhs.2,f.rhs.3)
+    
+    temp <- lp('min', f.obj, f.con, f.dir, f.rhs)
+    result.mat <- matrix(0,nrow=call.num,ncol=asset.num,dimnames=list(callId,assetId))
+    result.mat <- t(result.mat)
+    result.mat[idx.eli]<-temp$solution
+    result.mat <- t(result.mat)
+    
+    for(i in 1:call.num){
+      select.asset.idx <- which(result.mat[i,]!=0)
+      select.asset.name <- assetInfo$name[select.asset.idx]
+      select.asset.quantity <- result.mat[i,select.asset.idx]
+      select.asset.value <- value.mat[i,select.asset.idx]
+      select.asset.Amount <- select.asset.quantity*select.asset.value
+      select.asset.NetAmount <- select.asset.Amount*(1-haircut.mat[i,select.asset.idx])
+      select.asset.df <- data.frame(assetId[select.asset.idx],select.asset.name,select.asset.NetAmount,select.asset.Amount,select.asset.quantity)
+      colnames(select.asset.df)<- c('Asset','Name','NetAmount','Amount','quantity')
+      
+      select.list[[callId[i]]] <- select.asset.df       
+    }
     
   }
 }
   
-  return(output.list)
+  return(list(input.list=input.list,output.list=output.list))
 }
 
 
