4aa8e5b9642544587a271ab8f3f66aae76ad914a
silin yuan <silin yuan>
2016-12-01 16:54:36 +0800

    update

--- examples/allocationInputDataExamples.R
+++ examples/allocationInputDataExamples.R
@@ -3,7 +3,7 @@ library('RNeo4j')
 source('src/allocationInputData.R')
 
 allocationInputDataEx1 <- function(){
-  callId <- c('mc1','mc2','mc4')
+  callId <- c('mc1','mc2','mc5')
   clientId <- 'c1'
   result <- allocationInputData(callId,clientId)
   return(result)
--- examples/costOnlyNoConstraintAllocationAlgoExamples.R
+++ examples/costOnlyNoConstraintAllocationAlgoExamples.R
@@ -2,12 +2,18 @@ library("RNeo4j")
 source('src/functionsOfDBRequestByExecutingCypher.R')
 source('src/allocationFunction.R')
 
-#### CONSTANTS, PLASE DO NOT CHANGE #####
+#### CONSTANTS, PLEASE DO NOT CHANGE #####
 callId1 <- c('mc1','mc2','mc3','mc5'); clientId1 <- 'c1'
 callId2 <- c('mc1','mc2','mc3','mc4','mc5','mc6','mc7','mc8') ; clientId2 <- 'c1'
 
 modifyDBToSimulateCostOnlyNoConstraintAllocationAlgoCypherPath <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/test/testFiles/modifyAssetQuantityToSimulateCostOnlyNoConstraintAllocationAlgo.load'
 restoreDBDueToSimulateCostOnlyNoConstraintAllocationAlgoCypherPath <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/test/testFiles/restoreAssetQuantityDueToSimulateCostOnlyNoConstraintAllocationAlgo.load'
+
+modifyAssetQuantityCypherPath1 <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/test/testFiles/modify1AssetQuantityToSimulateCostOnlyQuantityLimitAllocationAlgo.load'
+restoreAssetQuantityCypherPath1 <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/test/testFiles/restore1AssetQuantityDueToSimulateCostOnlyQuantityLimitAllocationAlgo.load'
+
+modifyAssetCostCypherPath1 <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/test/testFiles/modify1AssetCostToTestAllocationAlgo.load'
+restoreAssetCostCypherPath1 <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/test/testFiles/restore1AssetCostToTestAllocationAlgo.load'
 
 #### EXAMPLE FUNCTIONS ##################
 costOnlyNoConstraintAllocationAlgoEx1 <- function(){
