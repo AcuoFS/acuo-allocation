6c79546c18f85682474105108d9c1c8cb7206ff5
silin yuan <silin yuan>
2016-12-07 13:35:32 +0800

    OW-201, update examples according to the change in DB

--- examples/callInfoByCallIdExamples.R
+++ examples/callInfoByCallIdExamples.R
@@ -5,6 +5,7 @@ source('src/functionsOfDBRequestByExecutingCypher.R')
 callId1 <- c('mc1')
 callId2 <- c('mc2','mc3')
 callId3 <- c('mc1','mc2','mc4','mc5','mc6','mc8','mc9','mc10','mc11')
+callId4 <- c('mc13','mc14','mc16','mc18','mc19','mc20')
 
 #### EXAMPLE FUNCTIONS ##################
 callInfoByCallIdEx1 <- function(){
@@ -16,8 +17,11 @@ callInfoByCallIdEx2 <- function(){
 callInfoByCallIdEx3 <- function(){
   callInfoByCallId(callId3)
 }
-
+callInfoByCallIdEx4 <- function(){
+  callInfoByCallId(callId4)
+}
 #### EXAMPLES RESULTS ####################
 callInfoByCallIdEx1()
-callInfoByCallIdEx1()
-callInfoByCallIdEx1()
+callInfoByCallIdEx2()
+callInfoByCallIdEx3()
+callInfoByCallIdEx4()
