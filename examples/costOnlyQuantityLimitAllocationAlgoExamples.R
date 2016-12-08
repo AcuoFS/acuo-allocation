8e6a4b1cbb953ddadba3db66e33e0cfdd9199113
silin yuan <silin yuan>
2016-12-08 18:49:16 +0800

    OW-174, update due to the separation of the cost

--- examples/costOnlyQuantityLimitAllocationAlgoExamples.R
+++ examples/costOnlyQuantityLimitAllocationAlgoExamples.R
@@ -4,25 +4,30 @@ source('src/allocationFunction.R')
 
 #### CONSTANTS, PLEASE DO NOT CHANGE #####
 callId1 <- c('mc1','mc2','mc3','mc5'); clientId1 <- 'c1'
-callId2 <- c('mc1','mc2','mc3','mc4','mc5','mc6','mc7','mc8') ; clientId2 <- 'c1'
+callId2 <- c('mc1','mc2','mc3','mc4','mc5','mc8') ; clientId2 <- 'c1'
+callId3 <- c('mc2','mc3','mc8','mc12','mc16') ; clientId3 <- 'c1'
 
 modifyAssetQuantityCypherPath1 <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/test/testFiles/modify1AssetQuantityToSimulateCostOnlyQuantityLimitAllocationAlgo.load'
 restoreAssetQuantityCypherPath1 <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/test/testFiles/restore1AssetQuantityDueToSimulateCostOnlyQuantityLimitAllocationAlgo.load'
 
-modifyAssetCostCypherPath1 <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/test/testFiles/modify1AssetCostToTestAllocationAlgo.load'
-restoreAssetCostCypherPath1 <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/test/testFiles/restore1AssetCostToTestAllocationAlgo.load'
+modifyAssetInternalCostCypherPath1 <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/test/testFiles/modify1AssetInternalCostToTestAllocationAlgo.load'
+restoreAssetIntenalCostCypherPath1 <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/test/testFiles/restore1AssetInternalCostToTestAllocationAlgo.load'
 
+modifyAssetExternalCostCypherPath1 <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/test/testFiles/modify1AssetExternalCostToTestAllocationAlgo.load'
+restoreAssetExtenalCostCypherPath1 <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/test/testFiles/restore1AssetExternalCostToTestAllocationAlgo.load'
 
 #### EXAMPLE FUNCTIONS ##################
 costOnlyQuantityLimitAllocationAlgoEx1 <- function(){
   
   executeCypher(path=modifyAssetQuantityCypherPath1)
-  executeCypher(path=modifyAssetCostCypherPath1)
+  executeCypher(path=modifyAssetInternalCostCypherPath1)
+  executeCypher(path=modifyAssetExternalCostCypherPath1)
   
-  allocation.result <- allocationAlgo(callId=callId1,clientId=clientId1,pref=c(0,0,1,0))
+  allocation.result <- allocationAlgo(callId=callId1,clientId=clientId1,pref=c(0,0,1))
   
   executeCypher(path=restoreAssetQuantityCypherPath1)
-  executeCypher(path=restoreAssetCostCypherPath1)
+  executeCypher(path=restoreAssetInternalCostCypherPath1)
+  executeCypher(path=restoreAssetExternalCostCypherPath1)
   
   return(allocation.result)
 }
@@ -30,12 +35,29 @@ costOnlyQuantityLimitAllocationAlgoEx1 <- function(){
 costOnlyQuantityLimitAllocationAlgoEx2 <- function(){
   
   executeCypher(path=modifyAssetQuantityCypherPath1)
-  executeCypher(path=modifyAssetCostCypherPath1)
+  executeCypher(path=modifyAssetInternalCostCypherPath1)
+  executeCypher(path=modifyAssetExternalCostCypherPath1)
   
-  allocation.result <- allocationAlgo(callId=callId2,clientId=clientId2,pref=c(0,0,1,0))
+  allocation.result <- allocationAlgo(callId=callId2,clientId=clientId2,pref=c(0,0,1))
   
   executeCypher(path=restoreAssetQuantityCypherPath1)
-  executeCypher(path=restoreAssetCostCypherPath1)
+  executeCypher(path=restoreAssetInternalCostCypherPath1)
+  executeCypher(path=restoreAssetExternalCostCypherPath1)
+  
+  return(allocation.result)
+}
+
+costOnlyQuantityLimitAllocationAlgoEx3 <- function(){
+  
+  executeCypher(path=modifyAssetQuantityCypherPath1)
+  executeCypher(path=modifyAssetInternalCostCypherPath1)
+  executeCypher(path=modifyAssetExternalCostCypherPath1)
+  
+  allocation.result <- allocationAlgo(callId=callId3,clientId=clientId3,pref=c(0,0,1))
+  
+  executeCypher(path=restoreAssetQuantityCypherPath1)
+  executeCypher(path=restoreAssetInternalCostCypherPath1)
+  executeCypher(path=restoreAssetExternalCostCypherPath1)
   
   return(allocation.result)
 }
@@ -43,5 +65,6 @@ costOnlyQuantityLimitAllocationAlgoEx2 <- function(){
 #### EXAMPLES RESULTS ####################
 costOnlyQuantityLimitAllocationAlgoEx1()
 costOnlyQuantityLimitAllocationAlgoEx2()
+costOnlyQuantityLimitAllocationAlgoEx3()
 
 
