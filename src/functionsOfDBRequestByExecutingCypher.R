48621df327b39def94cbdb059accdfaf513097e7
silin yuan <silin yuan>
2016-12-14 16:09:02 +0800

    OW-253, update the neo4j url

--- src/functionsOfDBRequestByExecutingCypher.R
+++ src/functionsOfDBRequestByExecutingCypher.R
@@ -1,6 +1,6 @@
 library('RNeo4j')
 
-neo4jUrl <- 'http://neo4j:7474/db/data'
+neo4jUrl <- 'http://neo4j.acuo.com:7474/db/data/'
 neo4jLocalUrl <- 'http://localhost:7474/db/data/'
 
 callInfoByCallIdCypherPath <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/src/Cypher/callInfoByCallId.cql'
@@ -12,8 +12,7 @@ assetInfoByAssetIdCypherPath <- 'https://raw.githubusercontent.com/AcuoFS/acuo-a
 executeCypher <- function(path,...){
   params <- list(...)
   query = paste(readLines(path), collapse="\n")
-  #graph = startGraph(neo4jUrl)
-  graph = startGraph(neo4jLocalUrl,username='neo4j',password='neo4j')
+  graph = startGraph(neo4jUrl)
   cypher(graph,query,params)
 }
   
