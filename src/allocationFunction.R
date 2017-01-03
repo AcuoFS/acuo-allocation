3faea34a81d5a028397552a2165610e68dd923d7
silin yuan <silin yuan>
2017-01-03 19:29:20 +0800

    OW-292, update allocation function

--- src/allocationFunction.R
+++ src/allocationFunction.R
@@ -98,18 +98,30 @@ for(i in 1:call.num){
 }
 operation.vec <- as.vector(t(operation.mat))
 
-norm.cost <- t(apply(cost.mat,1,scale))
-norm.liquidity <- t(apply(liquidity.mat,1,scale))
-norm.operation <- operation.mat
+norm.cost.mat <- cost.mat
 for(i in 1:call.num){
-  if(all(operation.mat[i,]==1)){
-    norm.operation[i,]<-0
+  if(length(unique(cost.mat[i,]))==1){
+    norm.cost.mat[i,]<-1
   }else{
-    norm.operation[i,]<- scale(operation.mat[i,])
+    norm.cost.mat[i,]<- scale(cost.mat[i,])
+    norm.cost.mat[i,]<- norm.cost.mat[i,]+(-min(norm.cost.mat[i,])*2)
   }
 }
+norm.cost.vec <- as.vector(t(norm.cost.mat))
+norm.liquidity.mat <- liquidity.mat
+for(i in 1:call.num){
+  if(length(unique(liquidity.mat[i,]))==1){
+    norm.liquidity.mat[i,]<-1
+  }else{
+    norm.liquidity.mat[i,]<- scale(liquidity.mat[i,])
+    norm.liquidity.mat[i,]<- norm.liquidity.mat[i,]+(-min(norm.liquidity.mat[i,])*2)
+  }
+}
+norm.liquidity.vec <- as.vector(t(norm.liquidity.mat))
+norm.operation.mat <- operation.mat*9+1
+norm.operation.vec <- as.vector(t(norm.operation.mat))
 
-optimal.mat <- norm.operation*pref[1]+norm.liquidity*pref[2]+norm.cost*pref[3]
+optimal.mat <- norm.operation.mat*pref[1]+norm.liquidity.mat*pref[2]+norm.cost.mat*pref[3]
 colnames(optimal.mat) <- assetId; rownames(optimal.mat)<-callId
 
 for(i in 1:call.num){
@@ -158,8 +170,141 @@ if(!is.element(0,suff.select.unique)){
   output.list<- select.list
 }
 
-
-else if(all(pref==c(0,0,1))){  # In case of OW-171,173,174, pref=(0,0,1)
+else if(1){
+  ##### In case of OW-292, consider all preference, with quantity limits ##########
+  
+  idx.eli <- which(eli.vec==1)  # Exclude the non-eligible asset variable for each margin call
+  var.num <- length(idx.eli)    # variable numbers
+  var.num2 <- var.num*2
+  
+  f.con.0 <- matrix(0,nrow=var.num2,ncol=var.num2)
+  f.con.0[cbind(1:var.num2,1:var.num2)] <- 1
+  f.dir.0 <- rep('>=',var.num2)
+  f.rhs.0 <- rep(0,var.num2)
+  
+  f.con.1 <- matrix(0,nrow=var.num2,ncol=var.num2)
+  f.con.1[cbind(1:var.num2,1:var.num)] <- 1
+  f.dir.1 <- rep('<=',var.num2)
+  f.rhs.1 <- c(eli.vec[idx.eli]*minUnitQuantity.vec[idx.eli],rep(1,var.num))
+  
+  
+  f.con.2 <- matrix(0,nrow=asset.num,ncol=var.num)
+  temp1 <- 1+(0:(call.num-1))*asset.num
+  idx.con.2 <- rep(temp1,asset.num)+rep(c(0:(asset.num-1)),rep(call.num,asset.num))
+  idx.con.2 <- match(idx.con.2,idx.eli)
+  f.con.2[na.omit(cbind(rep(c(1:asset.num),rep(call.num,asset.num)),idx.con.2))]<-1
+  f.con.2 <- cbind(f.con.2,f.con.2*0)
+  f.dir.2 <- rep('<=',asset.num)
+  f.rhs.2 <- minUnitQuantity.mat[1,]
+  
+  f.con.3 <- matrix(0,nrow=call.num,ncol=var.num)
+  idx.con.3 <- 1:(asset.num*call.num)
+  idx.con.3 <- match(idx.con.3,idx.eli)
+  f.con.3[na.omit(cbind(rep(c(1:call.num),rep(asset.num,call.num)),idx.con.3))] <- minUnitValue.vec[idx.eli]*(1-haircut.vec[idx.eli])
+  f.con.3 <- cbind(f.con.3,f.con.3*0)
+  f.dir.3 <- rep('>=',call.num)
+  f.rhs.3 <- call.mat[,1]
+  
+  f.con.4 <- matrix(0,nrow=var.num,ncol=var.num)
+  f.con.4[cbind(1:var.num,1:var.num)] <- 1
+  f.con.4 <- cbind(f.con.4,f.con.4*(-10000000000))
+  f.dir.4 <- rep('<=',var.num)
+  f.rhs.4 <- rep(0,var.num)
+  
+  f.con.5 <- matrix(0,nrow=var.num,ncol=var.num)
+  f.con.5[cbind(1:var.num,1:var.num)] <- 1
+  f.con.5 <- cbind(f.con.5,-f.con.5)
+  f.dir.5 <- rep('>=',var.num)
+  f.rhs.5 <- rep(0,var.num)
+  
+  operation.obj <-  c(rep(0,var.num),norm.operation.vec[idx.eli]*mean(call.mat))
+  liquidity.obj <-  c(minUnitValue.vec[idx.eli]*norm.liquidity.vec[idx.eli],rep(0,var.num))
+  cost.obj <-  c(minUnitValue.vec[idx.eli]*norm.cost.vec[idx.eli],rep(0,var.num))
+  
+  f.obj <- operation.obj*pref[1]+liquidity.obj*pref[2]+cost.obj*pref[3]
+  
+  names(f.obj) <- paste('var',1:var.num2)
+  
+  
+  # constraints
+  lp.con <- rbind(f.con.2,f.con.3,f.con.4,f.con.5)
+  lp.dir <- c(f.dir.2,f.dir.3,f.dir.4,f.dir.5)
+  lp.rhs <- c(f.rhs.2,f.rhs.3,f.rhs.4,f.rhs.5)
+  
+  lps.model <- make.lp(length(lp.con),var.num2)  # make model
+  set.objfn(lps.model,f.obj)                    # set objective
+  
+  for (i in 1:length(lp.con[,1])){              # set constraints
+    add.constraint(lps.model,lp.con[i,],lp.dir[i],lp.rhs[i])
+  }
+  
+  set.semicont(lps.model,1:var.num2,TRUE)        # set semi-continuous variables
+  
+  minMoveQuantity <- ceiling(minMoveValue/minUnitValue.vec[idx.eli])
+  if(length(call.vec[which(minMoveValue > call.vec[idx.eli]/(1-haircut.vec[idx.eli]))])!=0){
+    temp.idx <- which(minMoveValue > call.vec[idx.eli]/(1-haircut.vec[idx.eli]))
+    call.eli.vec <- call.vec[idx.eli]/(1-haircut.vec[idx.eli])
+    minUnitValue.eli.vec <- minUnitValue.vec[idx.eli]
+    minMoveQuantity[temp.idx] <- ceiling(call.eli.vec[temp.idx]/minUnitValue.eli.vec[temp.idx])
+  }
+  
+  # set integer constraint
+  # if the minUnitValue >=100, set to integer
+  int.idx <- which(minUnitValue.vec[idx.eli]>=100)
+  set.type(lps.model,int.idx,'integer')
+  set.bounds(lps.model,lower=c(minMoveQuantity,rep(1,var.num)),upper=c(minUnitQuantity.vec[idx.eli],rep(1,var.num)))
+  # set variables lower/upper bounds
+  lp.control(lps.model,epsd=1e-10,presolve='knapsack',timeout=5)
+  
+  result.status <- solve(lps.model)                              # solve model
+  
+  if(result.status==2){
+    errorMsg <- 'Error: Asset inventory might be insufficient!'
+    return(errorMsg)
+  } else if(result.status==5){                            # solve model
+    errorMsg <- 'Error: Fail to calculate!'
+    return(errorMsg)
+  } else if(result.status==1){
+    errorMsg <- 'sub-optimal result!'
+    return(errorMsg)
+  }
+  
+  lpSolveAPI.solution <- get.variables(lps.model)
+  
+  #cost.obj.result <- sum(cost.obj*lpSolveAPI.solution)
+  #liquidity.obj.result <- sum(liquidity.obj*lpSolveAPI.solution)
+  #operation.obj.result <- sum(operation.obj*lpSolveAPI.solution)
+  #total.move <- sum(lpSolveAPI.solution[(var.num+1):var.num2])
+  #obj.result <- c(cost.obj.result,liquidity.obj.result,operation.obj.result)
+  
+  result.mat <- matrix(0,nrow=call.num,ncol=asset.num,dimnames=list(callId,assetId))
+  result.mat <- t(result.mat)
+  result.mat[idx.eli]<-lpSolveAPI.solution[1:var.num]
+  result.mat <- t(result.mat)                   # convert solution into matrix format
+  
+  
+  for(i in 1:call.num){                          # store the result into select list
+    select.asset.idx <- which(result.mat[i,]!=0)
+    select.asset.id <- assetId[select.asset.idx]
+    select.asset.custodianAccount <- custodianAccount[select.asset.idx]
+    select.asset.venue <- venue[select.asset.idx]
+    select.asset.name <- assetInfo$name[select.asset.idx]
+    select.asset.haircut <- haircut.mat[i,select.asset.idx]
+    select.asset.currency <- assetInfo$currency[select.asset.idx]
+    select.asset.quantity <- result.mat[i,select.asset.idx]*minUnit.mat[i,select.asset.idx]
+    select.asset.unitValue <- unitValue.mat[i,select.asset.idx]
+    select.asset.Amount <- select.asset.quantity*select.asset.unitValue
+    select.asset.NetAmount <- select.asset.Amount*(1-haircut.mat[i,select.asset.idx])
+    
+    select.asset.df <- data.frame(select.asset.id,select.asset.name,select.asset.NetAmount,select.asset.haircut,select.asset.Amount,select.asset.currency,
+                                  select.asset.quantity,select.asset.custodianAccount,select.asset.venue)
+    colnames(select.asset.df)<- c('Asset','Name','NetAmount(USD)','Haircut','Amount','Currency','Quantity','CustodianAccount','venue')
+    
+    select.list[[callId[i]]] <- select.asset.df       
+  }
+}
+if(0){
+if(all(pref==c(0,0,1))){  # In case of OW-171,173,174, pref=(0,0,1)
   
 ##### In case of OW-174, all assets have quantity limits #######
    
@@ -311,7 +456,6 @@ else if(all(pref==c(0,0,1))){  # In case of OW-171,173,174, pref=(0,0,1)
     output.list <- select.list
   
 }
-
 else if(all(pref==c(0,1,0))){
   
  
@@ -424,7 +568,6 @@ else if(all(pref==c(0,1,0))){
   
   
 }
-
 else if(all(pref==c(1,0,0))){
   
   ##### In case of OW-254, all assets have quantity limits ##########
@@ -501,9 +644,8 @@ else if(all(pref==c(1,0,0))){
     }
     
     #  idx.int <- 1:var.num
-    set.type(lps.model,1:var.num2,type='integer') # set integer variables
+    #set.type(lps.model,1:var.num2,type='integer') # set integer variables
     set.semicont(lps.model,1:var.num2,TRUE)        # set semi-continuous variables
-    #set.type(lps.model,(var.num+1):var.num2,type="binary")
     
     minMoveQuantity <- ceiling(minMoveValue/minUnitValue.vec[idx.eli])
     
@@ -513,7 +655,7 @@ else if(all(pref==c(1,0,0))){
       minUnitValue.eli.vec <- minUnitValue.vec[idx.eli]
       minMoveQuantity[temp.idx] <- ceiling(call.eli.vec[temp.idx]/minUnitValue.eli.vec[temp.idx])
     }
-    set.bounds(lps.model,lower=c(minMoveQuantity,rep(0,var.num)),upper=c(minUnitQuantity.vec[idx.eli],rep(1,var.num)))
+    set.bounds(lps.model,lower=c(minMoveQuantity,rep(1,var.num)),upper=c(minUnitQuantity.vec[idx.eli],rep(1,var.num)))
     # set variables lower/upper bounds
     lp.control(lps.model,epsd=1e-10,presolve='knapsack',timeout=5)
     
@@ -570,7 +712,7 @@ else if(all(pref==c(1,0,0))){
   
   
 }
-
+}
   return(list(input=input.list,output=output.list))
 }
 
