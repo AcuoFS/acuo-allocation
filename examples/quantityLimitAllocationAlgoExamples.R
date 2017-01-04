fcf5f51b4ac325b5ab1526f621f67fc378f21c34
silin yuan <silin yuan>
2017-01-04 11:27:21 +0800

    OW-292, update examples

--- examples/quantityLimitAllocationAlgoExamples.R
+++ examples/quantityLimitAllocationAlgoExamples.R
@@ -5,7 +5,7 @@ source('src/allocationFunction.R')
 #### CONSTANTS, PLEASE DO NOT CHANGE #####
 callId1 <- c('mc41','mc42','mc43','mc45'); clientId1 <- '999'
 callId2 <- c('mc41','mc42','mc43','mc44','mc45','mc48') ; clientId2 <- '999'
-callId3 <- c('mc44','mc48','mc52','mc53','mc56') ; clientId3 <- '999'
+callId3 <- c('mc44','mc48','mc52','mc53') ; clientId3 <- '999'
 callId4 <- c('mc42','mc9','mc50','mc51','mc54','mc55','mc57','mc59','mc60') ; clientId4 <- '999'
 
 modifyAssetQuantityCypherPath1 <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/test/testFiles/modify1AssetQuantityToSimulatequantityLimitAllocationAlgo.load'
@@ -51,7 +51,7 @@ quantityLimitAllocationAlgoEx5 <- function(){
 }
 
 #### EXAMPLES RESULTS ####################
-quantityLimitAllocationAlgoEx1()
+quantityLimitAllocationAlgoEx1(
 quantityLimitAllocationAlgoEx2()
 quantityLimitAllocationAlgoEx3()
 quantityLimitAllocationAlgoEx4()
