5594480affff69a55e0cbddc615839f40a725d26
silin yuan <silin yuan>
2017-01-09 18:02:42 +0800

    OW-201, update cypher change the callType to marginType

--- examples/callInfoByCallIdExamples.R
+++ examples/callInfoByCallIdExamples.R
@@ -2,10 +2,12 @@ library('RNeo4j')
 source('src/functionsOfDBRequestByExecutingCypher.R')
 
 #### CONSTANTS, PLASE DO NOT CHANGE #####
-callId1 <- c('mc1')
-callId2 <- c('mc2','mc3')
-callId3 <- c('mc1','mc2','mc4','mc5','mc6','mc8','mc9','mc10','mc11')
-callId4 <- c('mc13','mc14','mc16','mc18','mc19','mc20')
+callId1 <- c('mc45')
+callId2 <- c('mc46','mc43')
+callId3 <- c('mc45','mc46','mc48','mc49','mc50','mc51')
+callId4 <- c('mc10','mc11','mc12','mc13','mc14','mc15','mc41','mc42','mc46','mc47','mc48','mc49',
+             'mc5','mc50','mc51','mc55','mc56','mc57','mc58','mc59',
+             'mc6','mc60','mc61','mc64') 
 
 #### EXAMPLE FUNCTIONS ##################
 callInfoByCallIdEx1 <- function(){
@@ -25,3 +27,4 @@ callInfoByCallIdEx1()
 callInfoByCallIdEx2()
 callInfoByCallIdEx3()
 callInfoByCallIdEx4()
+
