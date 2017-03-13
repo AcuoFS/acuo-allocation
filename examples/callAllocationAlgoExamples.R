80183f871c70ce7c757a5fdaf7c7d255534fdeda
silin yuan <silin yuan>
2017-03-13 19:41:04 +0800

    OW-480, update with the new variable names

--- examples/callAllocationAlgoExamples.R
+++ examples/callAllocationAlgoExamples.R
@@ -3,10 +3,10 @@
 # call ids
 # user id
 # uesr preference
-# callInfo <- callInfoByCallId(callIds)
-# availAssets <- availAssetByCallIdAndClientId(callIds,clientId)
-# assetIds
-# assetInfo <- assetInfoByAssetId(assetId)
+# callInfo_df <- callInfoByCallId(callId_vec)
+# availAsset_df <- availAssetByCallIdAndClientId(callId_vec,clientId)
+# assetId_vec
+# assetInfo_df <- assetInfoByAssetId(assetId)
 #######################################################################
 
 # Data below should come from the Java, 
@@ -16,39 +16,40 @@
 
 source('src/functionsOfDBRequestByExecutingCypher.R')
 source("src/allocationFunction.R")
+source("src/coreAlgo.R")
 
 # input #
-callIds = c("mcp45","mcp50","mcp43")
-callIds = c("mcp1","mcp5","mcp7","mcp50")
-callIds = c("mcp1","mcp5","mcp7","mcp38","mcp20","mcp22","mcp15","mcp20","mcp22","mcp30","mcp50","mcp51")
-callIds = c("mcp32","mcp33","mcp37","mcp26","mcp39","mcp50");
-callIds = c("mcp10")
+callId_vec = c("mcp45","mcp50","mcp43")
+callId_vec = c("mcp1","mcp5","mcp7","mcp50")
+callId_vec = c("mcp1","mcp5","mcp7","mcp38","mcp20","mcp22","mcp15","mcp20","mcp22","mcp30","mcp50","mcp51")
+callId_vec = c("mcp32","mcp33","mcp37","mcp26","mcp39","mcp50");
+callId_vec = c("mcp10")
 clientId = '999';
 
 # get info #
-callInfo <- callInfoByCallId(callIds)
-availAssets <- availAssetByCallIdAndClientId(callIds,clientId) # available asset for the margin call
-availAssets <- availAssets[order(availAssets$callId),]
+callInfo_df <- callInfoByCallId(callId_vec)
+availAsset_df <- availAssetByCallIdAndClientId(callId_vec,clientId) # available asset for the margin call
+availAsset_df <- availAsset_df[order(availAsset_df$callId),]
 
-# change quantity for testing
-availAssets$quantity <- availAssets$quantity/5
+# change tempQuantity_vec for testing
+availAsset_df$quantity <- availAsset_df$quantity/5
 # add custodianAccount for testing
-availAssets <- rbind(availAssets,availAssets)
-availAssets$CustodianAccount[1:length(availAssets[,1])/2] <- 'custodianAccountTest'
+availAsset_df <- rbind(availAsset_df,availAsset_df)
+availAsset_df$CustodianAccount[1:length(availAsset_df[,1])/2] <- 'custodianAccountTest'
 
-asset.custac.id <- paste(availAssets$assetId,availAssets$CustodianAccount,sep='-')
-availAssets$assetCustacId <- asset.custac.id
-assetCustacIds <- unique(asset.custac.id)
+assetCustacId_vec <- paste(availAsset_df$assetId,availAsset_df$CustodianAccount,sep='-')
+availAsset_df$assetCustacId <- assetCustacId_vec
+resource_vec <- unique(assetCustacId_vec)
 
-assetIds <- as.character(data.frame(strsplit(assetCustacIds,'-'))[1,])
-assetInfo <- assetInfoByAssetId(assetIds)
-assetInfo <- assetInfo[match(assetIds,assetInfo$id),]
+assetId_vec <- as.character(data.frame(strsplit(resource_vec,'-'))[1,])
+assetInfo_df <- assetInfoByAssetId(assetId_vec)
+assetInfo_df <- assetInfo_df[match(assetId_vec,assetInfo_df$id),]
 
 ## CALL THE ALLOCATION FUNCTION ###########
-call.limit <- c(7,7,7); time.limit=3
+callLimit_vec <- c(7,7,7); timeLimit=3
 #start.time <- proc.time()[3]
-pref = c(5,8,3);
-result <- allocationAlgo(callIds,assetCustacIds,callInfo,availAssets,assetInfo,pref,time.limit,call.limit)
+pref_vec = c(5,8,3);
+result <- AllocationAlgo(callId_vec,resource_vec,callInfo_df,availAsset_df,assetInfo_df,pref_vec,timeLimit,callLimit_vec)
 output <- result$output
 #end.time <- proc.time()[3]
 #run.time <- end.time-start.time
