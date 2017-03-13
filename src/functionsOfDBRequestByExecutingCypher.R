86542f6c882764da7e6568302ed60c6409e7b00a
silin yuan <silin yuan>
2017-03-13 13:02:15 +0800

    OW-498, use local DB

--- src/functionsOfDBRequestByExecutingCypher.R
+++ src/functionsOfDBRequestByExecutingCypher.R
@@ -2,7 +2,7 @@ library('RNeo4j')
 
 #neo4jUrl <- 'http://neo4j.acuo.com:7474/db/data/'
 neo4jUrl <-"http://neo4j:7474/db/data"
-neo4jLocalUrl = "http://neo4j:7474/db/data"
+neo4jLocalUrl = "http://localhost:7474/db/data"
 
 callInfoByCallIdCypherPath <- "https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/src/Cypher/callInfoByCallId.cql"
 availAssetByCallIdAndClientIdCypherPath <- 'https://raw.githubusercontent.com/AcuoFS/acuo-allocation/master/src/Cypher/availAssetByCallIdAndClientId.cql'
@@ -14,8 +14,8 @@ resAssetByClientIdCypherPath <- 'https://raw.githubusercontent.com/AcuoFS/acuo-a
 executeCypher <- function(path,...){
   params <- list(...)
   query = paste(readLines(path), collapse="\n")
-  graph = startGraph(neo4jUrl)
-  #graph = startGraph(neo4jLocalUrl,username='neo4j',password='neo4j')
+  #graph = startGraph(neo4jUrl)
+  graph = startGraph(neo4jLocalUrl,username='neo4j',password='neo4j')
   cypher(graph,query,params)
 }
   
