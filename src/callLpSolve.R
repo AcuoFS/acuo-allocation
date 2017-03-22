9706d526d43c6c1fdef8c3f9d46b22fd21b25b66
silin yuan <silin yuan>
2017-03-22 19:28:38 +0800

    update

--- src/callLpSolve.R
+++ src/callLpSolve.R
@@ -1,5 +1,5 @@
 CallLpSolve <- function(lpObj_vec,lpCon_mat,lpDir_vec,lpRhs_vec,
-                        lpType_vec=lpType_vec,lpKind_vec=lpKind_vec,lpLowerBound_vec=lpLowerBound_vec,lpUpperBound_vec=lpUpperBound_vec,lpBranchMode_vec=lpBranchMode_vec,
+                        lpType_vec,lpKind_vec,lpLowerBound_vec,lpUpperBound_vec,lpBranchMode_vec,
                         ...){
   library(lpSolveAPI)
   # input variables
