a9c077d837e3dd484916625bd452813c116ef667
silin yuan <silin yuan>
2017-04-26 13:47:23 +0800

    OW-544,test for second level algo

--- rscripts/secondRun.R
+++ rscripts/secondRun.R
@@ -8,7 +8,7 @@ dsAssetId <- assetId
 dsCallId_vec <- dsCallIds
 
 currentSelection_list <- selections
-print("currentSelection_list");print(typeof(selections),'\n');
+print("currentSelection_list");print(typeof(selections),'\n'); print(length(selections))
 print(currentSelection_list)
 callInfo_df <- callInfoByCallId
 callInfo_df$callAmount <- abs(as.numeric(callInfo_df$callAmount)) # make sure the callAmount is non-negative
