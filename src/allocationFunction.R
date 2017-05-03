384078a97d1d04cc7ec742a1f3d14b7960c1a304
silin yuan <silin yuan>
2017-05-03 12:01:08 +0800

    OW-551, change the functions location

--- src/allocationFunction.R
+++ src/allocationFunction.R
@@ -58,52 +58,33 @@ CallAllocation <- function(algoVersion,scenario,callId_vec,resource_vec,callInfo
 #### Main Function-Interface of Java End #########
 
 
-#### OTHER FUNCTIONS(CALLED IN THE MAIN FUNCTION)##########################
+#### CONNECT TO THE CORE MODULE OF OPTIMIZATION ####################
 AllocationAlgo <- function(callId_vec,resource_vec,resourceOri_vec,callInfo_df,availAsset_df,availAssetOri_df,assetInfo_df,assetInfoOri_df,pref_vec,operLimit,
                            algoVersion,minMoveValue,timeLimit,inputLimit_vec,callOrderMethod){
   
-  #### Order callId_vec Start ######################################
-  ## method 0: Keep original
-  ## method 1: By margin call amount, decreasing
-  ## method 2: By margin type, VM then IM; sub order by call amount
-  ## method 3: By total call amount in margin statement, decreasing
-  
-  if(missing(callOrderMethod)){
-    callOrderMethod <-2
-  }
-  
+  #### Input Prepare & Output Initialization Start ###########
+  #### Order callId_vec
   callInfo_df <- OrderCallId(callOrderMethod,callInfo_df)
   callId_vec <- callInfo_df$id
   msId_vec <- unique(callInfo_df$marginStatement)
-  #### Order callId_vec END ######################################
   
-  #### Group the callId_vec Start ################################
-  # method 1: group by marginType
-  # maximum limitVm VM or limitIm IM a time
-  # group by margin statements
-  imLimit <- inputLimit_vec[1]
-  vmLimit <- inputLimit_vec[2]  
-  callLimit <- inputLimit_vec[3]
-  msLimit <- inputLimit_vec[4]
+  #### Group the callId_vec
+  groupCallId_list <- GroupCallIdByMs(callLimit=inputLimit_vec[3],msLimit=inputLimit_vec[4],callInfo_df,callId_vec)
   
-  groupCallId_list <- GroupCallIdByMs(callLimit,msLimit,callInfo_df,callId_vec)
-  #### Group the callId_vec Start #################################
-  
-  
-  callNum <- length(callId_vec)
-  
-  # allocate one group a time
-  # after each allocation, update the tempQuantity_vec of each asset
+  #### Initialize the Ouput
   callOutput_list <- list()
   msOutput_list <- list()
   checkCall_mat <- matrix(c(callInfo_df$callAmount,rep(0,callNum)),nrow=callNum, dimnames = list(callId_vec,c('callAmount','fulfilledAmount')))
+  #### Input Prepare & Output Initialization END #############
   
   ############ ITERATE THE GROUP, RUN THE ALGO Start #########################
-  
+  callNum <- length(callId_vec)
+  msId_vec <- unique(callInfo_df$marginStatement)
   for(i in 1:length(groupCallId_list)){
-    ratio <- length(groupCallId_list[[i]])/length(callId_vec)
+    
     callIdGroup_vec <- groupCallId_list[[i]]
     msIdGroup_vec <- unique(callInfo_df$marginStatement[which(callInfo_df$id %in% callIdGroup_vec)])
+    ratio <- length(msIdGroup_vec[[i]])/length(msId_vec) # the proportion of the msGroup in the msList
     operLimitGroup <- operLimit*ratio
     #cat(' group:',i,'\n','callId_vec:',callIdGroup_vec,'\n')
     
@@ -254,13 +235,10 @@ AllocationAlgo <- function(callId_vec,resource_vec,resourceOri_vec,callInfo_df,a
 PreAllocation <- function(algoVersion,callIdGroup_vec,callInfo_df,availAsset_df,assetInfo_df,pref_vec,operLimit, minMoveValue,timeLimit,callOutput_list,checkCall_mat){
   
   msIdGroup_vec <- unique(callInfo_df$marginStatement[which(callInfo_df$id %in% callIdGroup_vec)])
-  #cat(' group:',i,'\n','callId_vec:',callIdGroup_vec,'\n')
   callInfoGroup_df <- callInfo_df[match(callIdGroup_vec,callInfo_df$id),]
-  #print('in pre function');print(availAsset_df)
   availAssetGroup_df <- availAsset_df[which(availAsset_df$callId %in% callIdGroup_vec),]
-  #print(availAssetGroup_df)
   resourceGroup_vec <- unique(availAssetGroup_df$assetCustacId)
-  assetIdGroup_vec <- matrix(unlist(strsplit(resourceGroup_vec,'-')),nrow=2)[1,]
+  assetIdGroup_vec <- SplitResource(resourceGroup_vec,'asset')
   assetInfoGroup_df <- assetInfo_df[match(assetIdGroup_vec,assetInfo_df$id),]
   
   # input data to the core Algo
@@ -294,110 +272,4 @@ PreAllocation <- function(algoVersion,callIdGroup_vec,callInfo_df,availAsset_df,
   
   solveOutput_list <- list(availAssetGroup_df=availAssetGroup_df,checkCall_mat=checkCall_mat,solverStatus=solverStatus,callOutput_list=callOutput_list)
   return(solveOutput_list)
-}
-
-OrderCallId <- function(callOrderMethod,callInfo_df){
-  if(callOrderMethod==0){ # keep original
-    callInfo_df <- callInfo_df
-  }else if(callOrderMethod==1){ # by call amount, decreasing
-    callInfo_df <- callInfo_df[order(callInfo_df$callAmount,decreasing=T),]
-  }else if(callOrderMethod==2){ # by margin type(VM first) and call amount, decreasing
-    callInfoVM <- callInfo_df[which(toupper(callInfo_df$marginType)=='VARIATION'),]
-    callInfoVM <- callInfoVM[order(callInfoVM$callAmount,decreasing=T),]
-    
-    callInfoIM <- callInfo_df[which(toupper(callInfo_df$marginType)=='INITIAL'),]
-    callInfoIM <- callInfoIM[order(callInfoIM$callAmount,decreasing=T),]
-    callInfo_df <- rbind(callInfoVM,callInfoIM)
-  }else if(callOrderMethod==3){ # by margin statement, call amount in margin statement, decreasing
-    msAggrCall_df <- aggregate(callAmount~marginStatement,data=callInfo_df,sum)
-    msAggrCall_df <- msAggrCall_df[order(msAggrCall_df$callAmount,decreasing=T),]
-    tempMs_vec <- msAggrCall_df$marginStatement
-    newCallInfo_df <- callInfo_df
-    idxCurrent <- 0
-    for(i in 1:length(tempMs_vec)){
-      idxTemp_vec <- which(tempMs_vec[i]==callInfo_df$marginStatement)
-      tempCallInfo_df <- callInfo_df[idxTemp_vec,]
-      tempCallInfo_df <- tempCallInfo_df[order(tempCallInfo_df$callAmount,decreasing=F),]
-      idxNewTemp_vec <- idxCurrent+1:length(idxTemp_vec)
-      newCallInfo_df[idxNewTemp_vec,] <- tempCallInfo_df
-      
-      idxCurrent <- idxCurrent+length(idxTemp_vec)
-    }
-    callInfo_df<- newCallInfo_df
-  }
-  return(callInfo_df)
-}
-
-SplitCallId <- function(vmLimit,imLimit,callLimit,msLimit,callInfo_df,callId_vec){
-  
-  groupCallId_list <- list()
-  # if the total call numbers is equal or less than limitTotal, only one group
-  if(length(callInfo_df[,1])<=limitTotal){
-    groupCallId_list[[1]] <- callId_vec
-  } else{
-    # index of VM and IM in the call list
-    
-    idxVm_vec <- which(toupper(callInfo_df$marginType)=='VARIATION')
-    idxIm_vec <- which(toupper(callInfo_df$marginType)=='INITIAL')
-    # number of VM and IM groups 
-    groupVmNum <- ceiling(length(idxVm_vec)/limitVm) 
-    groupImNum <- ceiling(length(idxIm_vec)/limitIm)
-    
-    # make the group list, VM and IM in the same list
-    index <- 0
-    if(groupVmNum==1){
-      index <- index+1
-      groupCallId_list[[index]] <- callId_vec[idxVm_vec]
-    } else if(groupVmNum > 1){
-      for(i in 1:(groupVmNum-1)){
-        index <- index+1
-        groupCallId_list[[index]] <- callId_vec[idxVm_vec[(i-1)*limitVm+(1:limitVm)]]
-      } 
-      index <- index+1
-      groupCallId_list[[index]] <- callId_vec[tail(idxVm_vec,length(idxVm_vec)-(groupVmNum-1)*limitVm)]
-    }
-    
-    if(groupImNum==1){
-      index <- index+1
-      groupCallId_list[[index]] <- callId_vec[idxIm_vec]
-    } else if(groupImNum > 1){
-      for(i in 1:(groupImNum-1)){
-        index <- index+1
-        groupCallId_list[[index]] <- callId_vec[idxIm_vec[(i-1)*limitIm+(1:limitIm)]]
-      } 
-      index <- index+1
-      groupCallId_list[[index]] <- callId_vec[tail(idxIm_vec,length(idxIm_vec)-(groupImNum-1)*limitIm)]
-    }
-  }
-  return(groupCallId_list)
-}
-
-GroupCallIdByMs <- function(callLimit,msLimit,callInfo_df,callId_vec){
-  
-  groupCallId_list <- list()
-  # if the total call numbers is equal or less than limitTotal, only one group
-  if(length(callInfo_df[,1])<=callLimit){
-    groupCallId_list[[1]] <- callId_vec
-  } else if(length(unique(callInfo_df$marginStatement))<=msLimit){
-    groupCallId_list[[1]] <- callId_vec
-  } else{
-    groupMsId_list <- list()
-    callMs_vec <- callInfo_df$marginStatement
-    ms_vec <- unique(callMs_vec)
-    msGroupNum <- ceiling(length(ms_vec)/msLimit)
-    
-    for(i in 1:(msGroupNum-1)){
-      tempCurrent <- msLimit*(i-1)
-      tempMs_vec <- ms_vec[(tempCurrent+1):(tempCurrent+msLimit)]
-      tempCall_vec <- callInfo_df$id[which((callInfo_df$marginStatement) %in% tempMs_vec)]
-      groupMsId_list[[i]]<- tempMs_vec
-      groupCallId_list[[i]]<- tempCall_vec
-    }
-    tempCurrent <- msLimit*(msGroupNum-1)
-    tempMs_vec <- na.omit(ms_vec[(tempCurrent+1):(tempCurrent+msLimit)])
-    tempCall_vec <- callInfo_df$id[which((callInfo_df$marginStatement) %in% tempMs_vec)]
-    groupMsId_list[[msGroupNum]]<- tempMs_vec
-    groupCallId_list[[msGroupNum]]<- tempCall_vec
-  }
-  return(groupCallId_list)
 }
