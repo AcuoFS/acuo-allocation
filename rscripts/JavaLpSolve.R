c70603e14bf5ebbf4c2a9cb42c2139046b888199
silin yuan <silin yuan>
2017-05-09 14:52:28 +0800

    OW-575, fixed the parameters passing to lpsolve through java interface

--- rscripts/JavaLpSolve.R
+++ rscripts/JavaLpSolve.R
@@ -145,7 +145,7 @@ CallLpSolve <- function(lpObj_vec,lpCon_mat,lpDir_vec,lpRhs_vec,
   }
   
   if(!missing(negrange)){
-    lpModel$setNegrange(negrange)
+    lpModel$setNegrange(as.integer(negrange))
   }
   
   if(!missing(lpGuessBasis_vec)){
