5ae1439e783395484e61eba3c4a542d0c6403304
silin yuan <silin yuan>
2017-05-30 19:51:42 +0800

    OW-608,fixed a typo in secondRun

--- rscripts/secondRun.R
+++ rscripts/secondRun.R
@@ -40,7 +40,7 @@ print(selections)
 selectedCallId_vec <- unlist(unique(selections$marginCall))
 
 if(!all(sort(unlist(callId_vec))==sort(selectedCallId_vec))){
-  errormsg <-  paste('callIds',paste(callId_vec,collapse = ' '),'not match the margin calls in selections',paste(temp,collapse = ' '))
+  errormsg <-  paste('callIds',paste(callId_vec,collapse = ' '),'not match the margin calls in selections',paste(selectedCallId_vec,collapse = ' '))
   stop(errormsg)
 }
 
