941259806e9c79a96e76033b8c3814f78cc5fa00
yuansilin <silin@acuo.com>
2017-12-12 18:55:01 +0800

    OW-933,update comments

--- src/substitution/manualSubstitution.R
+++ src/substitution/manualSubstitution.R
@@ -9,7 +9,7 @@ ManualSubstitution <- function(newResource,resource_df,availAsset_df,
   
   oldResourceAdjAmountUSD <- oldResourceRequireAmount/resource_df$FXRate[newIdx]*(1-subCollateral_df$haircut)
   
-  # calculate the amount required for new asset, the old amount is already in USD
+  # calculate the amount required for new asset
   newResourceMinQuantity <- ceiling(oldResourceAdjAmountUSD*resource_df$FXRate[newIdx]/(1-availAsset_df$haircut[newIdx]-availAsset_df$FXHaircut[newIdx])/resource_df$minUnitValue[newIdx])
   newResourceRequireAmount <- newResourceMinQuantity * resource_df$minUnitValue[newIdx]
   
@@ -19,14 +19,10 @@ ManualSubstitution <- function(newResource,resource_df,availAsset_df,
     warning("New asset is not sufficient for substituting!")
     return()
   } else{
-    # calculate the amount of new asset, the old amount is already in USD
-    #newResourceAmountUSD <- newResourceRequireAmount/resource_df$FXRate
-    #newResourceAmount <- newResourceAmountUSD*resource_df$FXRate[which(resource_df$id==newResource)]
-    
     # allocation for this asset, create AssetTransfer
-    newPortion_df <- ConstructNewAllocDf(newResource,newResourceRequireAmount,resource_df,availAsset_df)
+    newAssetTransfer_df <- ConstructNewAllocDf(newResource,newResourceRequireAmount,resource_df,availAsset_df)
     
-    return(newPortion_df)
+    return(newAssetTransfer_df)
   }
 }
 
