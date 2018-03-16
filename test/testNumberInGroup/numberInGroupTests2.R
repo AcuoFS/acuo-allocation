93e7980b1bbe6834167b477f55d4b5a4c57f804f
yuansilin <silin@acuo.com>
2018-03-16 11:06:09 +0800

    OW-972, added the tests

---
+++ test/testNumberInGroup/numberInGroupTests2.R
@@ -0,0 +1,100 @@
+
+source("src/generalFunctions.R")
+source("src/allocationFunction.R")
+source("src/coreAlgo.R")
+source("src/callLpSolve.R")
+library("testthat")
+library("XLConnect")
+
+filePath <- "test/testNumberInGroup/numberInGroupPerformance.xlsx"
+
+
+testCallNumber10AssetNumberS10M35L5Group1 <- function(){
+  load("test/testAssetAmount/callNumber10AssetNumberS10M35L5.RData")
+  inputLimit_vec <- c(1,1,1,1)
+  timeLimit <- 10
+  worksheet <- readWorksheetFromFile(filePath,sheet="Results")
+  ptm <- proc.time()
+  result <- CallAllocation(algoVersion,scenario=1,callInfo_df,availAsset_df,resource_df,
+                           pref_vec,operLimit,operLimitMs_vec,fungible,
+                           ifNewAlloc=T,list(),inputLimit_vec,timeLimit,callOrderMethod,minMoveValue)
+  
+  temp <- proc.time() - ptm
+  runTime <- temp[3]
+  analysis <- result$resultAnalysis
+  output <- cbind("callNumber10AssetNumberS10M35L5.RData",inputLimit_vec[4],timeLimit,runTime,analysis$dailyCost,analysis$reservedLiquidityRatio,analysis$movements)
+  writeWorksheetToFile(filePath,data=output,sheet='Results',startRow=length(worksheet[,1])+2,startCol=1,header=F)
+  
+}
+
+testCallNumber10AssetNumberS10M35L5Group2 <- function(){
+  load("test/testAssetAmount/callNumber10AssetNumberS10M35L5.RData")
+  inputLimit_vec <- c(4,4,4,2)
+  timeLimit <- 10*2
+  worksheet <- readWorksheetFromFile(filePath,sheet="Results")
+  ptm <- proc.time()
+  result <- CallAllocation(algoVersion,scenario=1,callInfo_df,availAsset_df,resource_df,
+                           pref_vec,operLimit,operLimitMs_vec,fungible,
+                           ifNewAlloc=T,list(),inputLimit_vec,timeLimit,callOrderMethod,minMoveValue)
+  
+  temp <- proc.time() - ptm
+  runTime <- temp[3]
+  analysis <- result$resultAnalysis
+  output <- cbind("callNumber10AssetNumberS10M35L5.RData",inputLimit_vec[4],timeLimit,runTime,analysis$dailyCost,analysis$reservedLiquidityRatio,analysis$movements)
+  writeWorksheetToFile(filePath,data=output,sheet='Results',startRow=length(worksheet[,1])+2,startCol=1,header=F)
+  
+}
+
+testCallNumber10AssetNumberS10M35L5Group3 <- function(){
+  load("test/testAssetAmount/callNumber10AssetNumberS10M35L5.RData")
+  inputLimit_vec <- c(6,6,6,3)
+  timeLimit <- 10*3
+  worksheet <- readWorksheetFromFile(filePath,sheet="Results")
+  ptm <- proc.time()
+  result <- CallAllocation(algoVersion,scenario=1,callInfo_df,availAsset_df,resource_df,
+                           pref_vec,operLimit,operLimitMs_vec,fungible,
+                           ifNewAlloc=T,list(),inputLimit_vec,timeLimit,callOrderMethod,minMoveValue)
+  
+  temp <- proc.time() - ptm
+  runTime <- temp[3]
+  analysis <- result$resultAnalysis
+  output <- cbind("callNumber10AssetNumberS10M35L5.RData",inputLimit_vec[4],timeLimit,runTime,analysis$dailyCost,analysis$reservedLiquidityRatio,analysis$movements)
+  writeWorksheetToFile(filePath,data=output,sheet='Results',startRow=length(worksheet[,1])+2,startCol=1,header=F)
+  
+}
+
+testCallNumber10AssetNumberS10M35L5Group4 <- function(){
+  load("test/testAssetAmount/callNumber10AssetNumberS10M35L5.RData")
+  inputLimit_vec <- c(8,8,8,4)
+  timeLimit <- 10*4
+  worksheet <- readWorksheetFromFile(filePath,sheet="Results")
+  ptm <- proc.time()
+  result <- CallAllocation(algoVersion,scenario=1,callInfo_df,availAsset_df,resource_df,
+                           pref_vec,operLimit,operLimitMs_vec,fungible,
+                           ifNewAlloc=T,list(),inputLimit_vec,timeLimit,callOrderMethod,minMoveValue)
+  
+  temp <- proc.time() - ptm
+  runTime <- temp[3]
+  analysis <- result$resultAnalysis
+  output <- cbind("callNumber10AssetNumberS10M35L5.RData",inputLimit_vec[4],timeLimit,runTime,analysis$dailyCost,analysis$reservedLiquidityRatio,analysis$movements)
+  writeWorksheetToFile(filePath,data=output,sheet='Results',startRow=length(worksheet[,1])+2,startCol=1,header=F)
+  
+}
+
+testCallNumber10AssetNumberS10M35L5Group5 <- function(){
+  load("test/testAssetAmount/callNumber10AssetNumberS10M35L5.RData")
+  inputLimit_vec <- c(10,10,10,5)
+  timeLimit <- 10*5
+  worksheet <- readWorksheetFromFile(filePath,sheet="Results")
+  ptm <- proc.time()
+  result <- CallAllocation(algoVersion,scenario=1,callInfo_df,availAsset_df,resource_df,
+                           pref_vec,operLimit,operLimitMs_vec,fungible,
+                           ifNewAlloc=T,list(),inputLimit_vec,timeLimit,callOrderMethod,minMoveValue)
+  
+  temp <- proc.time() - ptm
+  runTime <- temp[3]
+  analysis <- result$resultAnalysis
+  output <- cbind("callNumber10AssetNumberS10M35L5.RData",inputLimit_vec[4],timeLimit,runTime,analysis$dailyCost,analysis$reservedLiquidityRatio,analysis$movements)
+  writeWorksheetToFile(filePath,data=output,sheet='Results',startRow=length(worksheet[,1])+2,startCol=1,header=F)
+  
+}
