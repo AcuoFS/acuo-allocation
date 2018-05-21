e5520bad23503d7961e45c9e69b14cea2a57062f
yuansilin <silin@acuo.com>
2018-05-21 13:37:59 +0800

    OW-1008, refactoring

--- src/allocationApproaches.R
+++ src/allocationApproaches.R
@@ -31,19 +31,22 @@ AllocateUnderInsufficientOptimalAssets <- function(costScore_mat,liquidityScore_
   # Returns:
   #   A matrix of the allocated quantity by each call and each resource
   
-  idxEli_vec <- which(t(EliMat(availAsset_df,callInfo_df$id,resource_df$id))==1)         # eligible index
+  #### Eligibility Index in Matrix ####
+  # to get the eligible entries in the matrix data
+  idxEli_vec <- which(t(EliMat(availAsset_df,callInfo_df$id,resource_df$id))==1)
   
+  #### Deduct Quantity in resource_df ####
+  # deduct some units in to avoid overflow in the rounding later 
+  oriResourceQtyMin_vec <- resource_df$qtyMin
+  resource_df$qtyMin <- resource_df$qtyMin - max(1,(length(callInfo_df$id)-1))
+  
+  #### Resuable Variables ####
   resourceNum <- length(resource_df$id)
   callNum <- length(callInfo_df$id)
-  
   callAmount_vec <- matrix(rep(callInfo_df$callAmount,resourceNum),nrow=resourceNum,byrow=T)[idxEli_vec]
-  
   quantity_vec <- matrix(rep(resource_df$qtyMin,callNum),nrow=callNum)[idxEli_vec]
-  
   minUnit_vec <- matrix(rep(resource_df$minUnit, callNum),nrow=callNum)[idxEli_vec]
-  
   minUnitValue_vec <- matrix(rep(resource_df$minUnitValue, callNum),nrow=callNum)[idxEli_vec]
-  
   haircut_vec <- availAsset_df$haircut + availAsset_df$FXHaircut
   
   #### Construct Variable Names ######
@@ -63,10 +66,6 @@ AllocateUnderInsufficientOptimalAssets <- function(costScore_mat,liquidityScore_
   names(fObj_vec) <- varName_vec
   
   #### CONSTRAINTS
-  
-  # deduct some units to avoid overflow in the rounding later
-  oriQuantity_vec <- quantity_vec
-  quantity_vec <- quantity_vec - max(1,(callNum-1))
   
   fCon2_list <- QtyConst(varName_vec,varNum,resource_df$id,resource_df$qtyMin)
   fCon3_list <- MarginConst(varName_vec,varNum,minUnitValue_vec,haircut_vec,callInfo_df$id,callInfo_df$callAmount)
@@ -105,19 +104,16 @@ AllocateUnderInsufficientOptimalAssets <- function(costScore_mat,liquidityScore_
     lpGuessBasis_vec <- ResultList2Vec(initAllocation_list,callInfo_df$id,minUnit_vec,varName_vec,varNum,pos_vec)
   }
   
-  #### Solve the Model ###############
-  #### call lpSolve solver
+  #### Call Solver to Solve the Model ###############
   solverOutput_list <- CallLpSolve(lpObj_vec,lpCon_mat,lpDir_vec,lpRhs_vec,
                                    lpType_vec=lpType_vec,lpKind_vec=lpKind_vec,lpLowerBound_vec=lpLowerBound_vec,lpUpperBound_vec=lpUpperBound_vec,lpBranchMode_vec=lpBranchMode_vec,
                                    lpGuessBasis_vec=lpGuessBasis_vec, 
                                    presolve=lpPresolve,timeout=lpTimeout)
-  #### solver outputs
   solverStatus<- solverOutput_list$resultStatus
   solverSolution_vec <- solverOutput_list$solverSolution_vec
   solverObjValue <- solverOutput_list$solverObjValue
   
-  
-  #### Exception Handling ####
+  #### Solver Exception Handling ####
   errStatus <- c(2,5,6,7,10,13)
   if(solverStatus==2){
     errormsg <- paste("ALERR2005: The model constructed by margin calls",paste(callInfo_df$id,collapse = " "),"is infeasible")
@@ -127,12 +123,13 @@ AllocateUnderInsufficientOptimalAssets <- function(costScore_mat,liquidityScore_
       rank_vec <- CalculateObjParams(t(costScore_mat)[idxEli_vec],t(liquidityScore_mat)[idxEli_vec],pref_vec,"amount",minUnitValue_vec)
       solverSolution_vec <- AllocateByRank(resource_df$id,callInfo_df$id,rank_vec,callInfo_df$callAmount,quantity_vec,minUnitValue_vec,haircut_vec,operLimit)
     } else{ # Solver time out
-      #### choose the best alternative
+      ## choose the best alternative
       solverSolution_vec <- lpGuessBasis_vec
     }
   }
   
-  quantity_vec <- oriQuantity_vec
+  #### Restore Quantity in resource_df ####
+  resource_df$qtyMin <- oriResourceQtyMin_vec
   
   #### Adjust & Convert the Solver Result Start ######
   solverSolution_vec <- AdjustResultVec(solverSolution_vec,varNum,varName_vec,fCon4_list$coef_mat,
